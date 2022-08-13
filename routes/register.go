f519b30242b146d38b0e1070c91ecbbb2ad4e42a
warren yun <wyun2006@gmail.com>
2022-08-13 19:39:28 -0400

    register directory

--- routes/register.go
+++ routes/register.go
@@ -5,7 +5,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type DirectoryRequestBody struct {
+	Directory 	string
+}
+
 func StartRegister(group *gin.RouterGroup) {
+
+}
+
+func RegisterDirectory(c *gin.Context) {
+	var reqBody DirectoryRequestBody;
+	if err := c.Copy().ShouldBindJSON(&reqBody); err != nil {
+		fmt.Println(err);
+	}
+	fmt.Println(reqBody.Directory);
 
 }
 
