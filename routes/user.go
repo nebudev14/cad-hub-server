209bccd9ee8b0a44dd0493cc6e29e19bbf1125b2
warren yun <wyun2006@gmail.com>
2022-08-13 22:04:53 -0400

    create user route

--- routes/user.go
+++ routes/user.go
@@ -2,22 +2,33 @@ package routes
 
 import (
 	"context"
-	"fmt"
-
 	"github.com/NebuDev14/cad-hub-server/db"
 	"github.com/NebuDev14/cad-hub-server/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
 )
 
 var userCollection *mongo.Collection
 
 func StartUsers(group *gin.RouterGroup) {
-	userCollection =  db.GetDB().Collection("users")
+	userCollection = db.GetDB().Collection("users")
 	group.GET("/:id", GetById)
+	group.POST("/:name", CreateUser)
+}
+
+func CreateUser(c *gin.Context) {
+	name := c.Param("name")
+	newUser := models.User{Name: name}
+
+	createNew, err := userCollection.InsertOne(context.TODO(), newUser)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error: ": err.Error()})
+	}
+
+	c.JSON(http.StatusOK, createNew)
 }
 
 func GetById(c *gin.Context) {
@@ -34,5 +45,4 @@ func GetById(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, result)
-
 }
