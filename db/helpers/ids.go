7f8c80e8ad5aabb38a7df00e320473c64e3fb5bc
warren yun <wyun2006@gmail.com>
2022-08-13 23:12:14 -0400

    proper error handling rofl

--- db/helpers/ids.go
+++ db/helpers/ids.go
@@ -2,25 +2,23 @@ package helpers
 
 import (
 	"context"
-	"fmt"
-
 	"github.com/NebuDev14/cad-hub-server/models"
 	"github.com/NebuDev14/cad-hub-server/db"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func GetUserById(id string) models.User {
+func GetUserById(id string) (models.User, error) {
 	objId, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
-		fmt.Println(err)
+		return models.User{}, err
 	}
 	filter := bson.D{{"_id", objId}}
 
 	var result models.User
 	if err := db.GetDB().Collection("users").FindOne(context.TODO(), filter).Decode(&result); err != nil {
-		fmt.Println(err)
+		return models.User{}, err
 	}
 
-	return result
+	return result, nil
 }
--- routes/projects.go
+++ routes/projects.go
@@ -1,35 +1,35 @@
 package routes
 
-// import (
-// 	"context"
-// 	"github.com/NebuDev14/cad-hub-server/db"
-// 	"github.com/NebuDev14/cad-hub-server/models"
-// 	"github.com/gin-gonic/gin"
-// 	"go.mongodb.org/mongo-driver/bson"
-// 	"go.mongodb.org/mongo-driver/bson/primitive"
-// 	"go.mongodb.org/mongo-driver/mongo"
-// 	"net/http"
-// )
+import (
+	"context"
+	"github.com/NebuDev14/cad-hub-server/db"
+	"github.com/NebuDev14/cad-hub-server/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"net/http"
+)
 
-// var projectCollection *mongo.Collection
+var projectCollection *mongo.Collection
 
-// type CreateProjectReq struct {
-// 	UserId		string
-// 	name			string
-// }
+type CreateProjectReq struct {
+	UserId		string
+	name			string
+}
 
-// func StartProjects(group *gin.RouterGroup) {
-// 	projectCollection = db.GetDB().Collection("projects")
-// 	group.POST("/:id", CreateProject)
-// }
+func StartProjects(group *gin.RouterGroup) {
+	projectCollection = db.GetDB().Collection("projects")
+	group.POST("/:id", CreateProject)
+}
 
-// func CreateProject(c *gin.Context) {
-// 	var reqBody CreateProjectReq
-// 	if err := c.Copy().ShouldBindJSON(&reqBody); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error: ": err.Error()})
-// 	}
+func CreateProject(c *gin.Context) {
+	var reqBody CreateProjectReq
+	if err := c.Copy().ShouldBindJSON(&reqBody); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error: ": err.Error()})
+	}
 
 
-// 	// newProject := models.Project{Id: primitive.NewObjectID(), Name: reqBody.name, Users: []}
+	newProject := models.Project{Id: primitive.NewObjectID(), Name: reqBody.name, Users: []}
 
-// }
+}
